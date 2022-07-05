0948a55e06cc00e4e41a226b302ecc6822f17443
Tuukka <hartikainentuukka@gmail.com>
2022-07-05 22:30:54 +0300

    koodi refaktoroitu

--- index.js
+++ index.js
@@ -1,43 +1,15 @@
-const http = require('http')
-const express = require('express')
-const app = express()
-const cors = require('cors')
+const app = require('./app')
 const mongoose = require('mongoose')
+const http = require('http')
 
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
 
 const mongoUrl = 'mongodb+srv://fullstack:UYkzigx6o9LufkXP@cluster0.slqqj.mongodb.net/blogiLista?retryWrites=true&w=majority'
 mongoose.connect(mongoUrl)
 
-app.use(cors())
-app.use(express.json())
+const server = http.createServer(app)
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
 
 const PORT = 3003
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
