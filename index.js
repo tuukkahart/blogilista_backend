0af3503eaa302358a77d10ea703e2222602e9450
Tuukka <hartikainentuukka@gmail.com>
2022-07-06 11:16:35 +0300

    addedd .env

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 const app = require('./app')
 const mongoose = require('mongoose')
 const http = require('http')
+require('dotenv').config()
 
 
-const mongoUrl = 'mongodb+srv://fullstack:UYkzigx6o9LufkXP@cluster0.slqqj.mongodb.net/blogiLista?retryWrites=true&w=majority'
+const mongoUrl = process.env.MONGODB_URI
 mongoose.connect(mongoUrl)
 
 const server = http.createServer(app)
 
 
-const PORT = 3003
+const PORT = process.env.PORT
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
