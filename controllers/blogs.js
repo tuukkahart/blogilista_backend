e1f501c153c6aea5546cb7124f8dcf19490904b2
Tuukka <hartikainentuukka@gmail.com>
2022-07-28 14:15:34 +0300

    fixed problem that cahnges user when blog is liked

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,6 +1,15 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+
+const getTokenFrom = request => {
+    const authorization = request.get('authorization')
+    if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+        return authorization.substring(7)
+    }
+    return null
+}
 
 blogsRouter.get('/', async (request, response) => {
     const blogs = await Blog.find({}).populate('user', {username: 1 , name: 1 })
@@ -11,7 +20,12 @@ blogsRouter.get('/', async (request, response) => {
 blogsRouter.post('/', async (request, response) => {
     const body = request.body
 
-    const user = await User.findById(body.userId)
+    const token = getTokenFrom(request)
+    const decodedToken = jwt.verify(token, process.env.SECRET)
+    if (!token || !decodedToken.id) {
+        return response.status(401).json({ error: 'token missing or invalid' })
+    }
+    const user = await User.findById(decodedToken.id)
 
     const blog = new Blog({
       title: body.title,
@@ -28,6 +42,28 @@ blogsRouter.post('/', async (request, response) => {
 
     response.json(savedBlog)
     
+})
+
+blogsRouter.put('/:id', async (request, response) => {
+    const body = request.body
+    const token = getTokenFrom(request)
+    const decodedToken = jwt.verify(token, process.env.SECRET)
+    if (!token || !decodedToken.id) {
+        return response.status(401).json({ error: 'token missing or invalid' })
+    }
+    const user = await User.findById(decodedToken.id)
+
+    const blog = {
+        title: body.title,
+        author: body.author,
+        url: body.url,
+        likes: body.likes,
+        user: body.user.id
+    }
+
+    const changedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, {new: true})
+    response.json(changedBlog)
+
 })
 
 blogsRouter.delete('/:id', async (request, response) => {
